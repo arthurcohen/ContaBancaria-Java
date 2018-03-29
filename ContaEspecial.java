1dbbd9f6369acf6dbfe2abeb99365b5099cf79cb
Arthur Cohen <arthu@PeDePano.pedepano>
2018-03-29 10:46:19 -0300

    created the basis of ContaEspecial and ContaPoupanca

---
+++ ContaEspecial.java
@@ -0,0 +1,20 @@
+public class ContaEspecial extends ContaBancaria{
+    private double limite;
+
+    public ContaEspecial(String cliente, int numConta, double saldo, double limite){
+        super(cliente, numConta, saldo);
+        this.limite = limite;
+    }
+
+    public boolean sacar(double valor){
+        if ((valor > 0) && (this.saldo - valor >= limite * -1)){
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public String toString(){
+        return String.format("Cliente %s\n%d - R$ %d (limite da conta R$ %d)", super.cliente, this.numConta, this.saldo, this.limite);
+    }
+}
---
+++ ContaPoupanca.java
@@ -0,0 +1,16 @@
+public class ContaPoupanca extends ContaBancaria{
+    private int diaDeRendimento;
+
+    public ContaPoupanca(String cliente, int numConta, double saldo, int diaDeRendimento){
+        super(cliente, numConta, saldo);
+        this.diaDeRendimento = diaDeRendimento;
+    }
+
+    public void CalcularNovoSaldo(double taxaRendimento){
+        this.saldo += this.saldo * (taxaRendimento/100.0);
+    }
+
+    public String toString(){
+        return String.format("Cliente %s\n%d - R$ %d (dia de rendimento %d)", super.cliente, this.numConta, this.saldo, this.diaDeRendimento);
+    }
+}
