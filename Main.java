3e59974d9cac832dd3f035b2a8a38ffc2425bf48
Arthur Cohen <arthu@PeDePano.pedepano>
2018-03-29 13:05:42 -0300

    some new tests

--- Main.java
+++ Main.java
@@ -1,14 +1,28 @@
 public class Main{
     public static void main(String[] args) {
         Banco banco = new Banco();
+
         banco.adicionarConta(new ContaPoupanca("cliente 1", 1, 500.0, 5));
         banco.adicionarConta(new ContaEspecial("cliente 2", 2, 200.0, 50.0));
 
         System.out.println(banco.mostarTodasContas());
 
-        System.out.println(banco.getConta(1) + "\n");
+        // System.out.println(banco.getConta(1) + "\n");
 
         banco.removerConta(1);
+
+        System.out.println(banco.mostarTodasContas());
+
+        banco.CalcularNovoSaldo(0, 10);
+        banco.sacar(0, 500.0);
+
+        System.out.println(banco.mostarTodasContas());
+
+        banco.removerConta(0);
+        banco.adicionarConta(new ContaEspecial("cliente 2", 2, 200.0, 50.0));
+        
+        banco.sacar(0, 250.0);
+        banco.CalcularNovoSaldo(0, 10);
 
         System.out.println(banco.mostarTodasContas());
     }
